af286f2db2f7668bf755793eed8bd6548505d885
Denver McCarthy <dmichaelspdx@gmail.com>
2022-01-19 17:58:15 -0800

    added event listener

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const container = document.querySelector('.profile');
 
 
 // set event listeners 
-    // get user input
+selectEl.addEventListener('change', (e) => {
+    hobbiesEl.innerHTML = '';
+    container.classList.remove('denver', 'brenden');
+    const selected = e.target.value; 
+    if (selected === 'one') {
+        renderPerson(0);
+    } else {
+        renderPerson(1);
+    }
+});
+
+
+
     // use user input to update state 
     // update DOM to reflect the new state
