a454f18d94a85c9eb51d5745a47be383a45e547c
Denver McCarthy <dmichaelspdx@gmail.com>
2022-01-19 17:32:07 -0800

    added classes and ids in html, div display, and element variables in javascript

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // import functions
+import { people } from './data.js';
 
 // grab DOM elements
+const selectEl = document.getElementById('name');
+const nameEl = document.querySelector('.name');
+const ageEl = document.querySelector('.age');
+const bioEl = document.querySelector('.bio');
+const hobbiesEl = document.querySelector('.hobbies');
+const container = document.querySelector('.profile');
+
 
 // set event listeners 
     // get user input
