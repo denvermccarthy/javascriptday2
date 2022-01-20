a861ae5ce9a17c27a54b5637ad30058cd8f9371b
Denver McCarthy <dmichaelspdx@gmail.com>
2022-01-19 16:45:19 -0800

    added data to data.js and a selector menu

---
+++ data.js
@@ -0,0 +1,14 @@
+export const people = [
+{id: 1,
+name: 'Brenden',
+age: 29,
+bio: 'lives in washington',
+hobbies: ['bad movies', 'guitar']
+}, {
+  id: 2,
+  name: 'Denver',
+  age: 18,
+  bio: 'lives in oregon',
+  hobbies: ['camping', 'baking']
+}
+]
